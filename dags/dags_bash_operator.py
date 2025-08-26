40b425bd824eba8426eda9def19748e832b9b92e
hyeon424 <cold3311@gmail.com>
2025-08-26 14:15:28 +0900

    bash_operator dag 수정

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -7,7 +7,7 @@ from airflow.providers.standard.operators.bash import BashOperator
 
 with DAG(
     dag_id='dags_bash_operator',  # DAG 파일명과 ID는 일치시키는 것이 권장됨
-    schedule_interval='0 0 * * *',
+    schedule='0 0 * * *',
 
     # start_date와 catchup은 같이 보아야 함
     start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
@@ -25,7 +25,7 @@ with DAG(
 
     bash_task2 = BashOperator(
         task_id='bash_task2',
-        bash_command='echo $HOSTNAME',
+        bash_command='echo $HOSTNAME', # 해당 task 실행의 출력 결과를 통해 worker container가 수행했음을 알 수 있음
     )
     
     bash_task1 >> bash_task2
