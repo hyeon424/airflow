fb9856e8cf728092c9692cf8636717d229506dc5
hyeon424 <cold3311@gmail.com>
2025-08-26 13:46:06 +0900

    bash_operator dag upload

---
+++ dags/dags_bash_operator.py
@@ -0,0 +1,31 @@
+import datetime
+
+import pendulum
+
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+
+with DAG(
+    dag_id='dags_bash_operator',  # DAG 파일명과 ID는 일치시키는 것이 권장됨
+    schedule_interval='0 0 * * *',
+
+    # start_date와 catchup은 같이 보아야 함
+    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
+    catchup=False,  # 일반적으로는 False가 default
+
+    # dagrun_timeout=datetime.timedelta(minutes=60), # timeout되면 stop시킴
+    # tags=['example', 'example2'],
+    # params={"example_key": "example_value"},
+) as dag:
+    # [START howto_operator_bash]
+    bash_task1 = BashOperator(
+        task_id='bash_task1', # DAG와 마찬가지로 일치시키는 것이 권장됨
+        bash_command='echo whoami',
+    )
+
+    bash_task2 = BashOperator(
+        task_id='bash_task2',
+        bash_command='echo $HOSTNAME',
+    )
+    
+    bash_task1 >> bash_task2
