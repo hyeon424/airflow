4fcf0d70685fdcf7fb7c5074408c9d41b243fe72
hyeon424 <cold3311@gmail.com>
2025-08-27 16:23:27 +0900

    dags_bash_with_macro_ex1 & ex2 retry4

--- dags/dags_bash_with_macro_ex2.py
+++ dags/dags_bash_with_macro_ex2.py
@@ -18,9 +18,9 @@ with DAG(
         task_id="bash_task_2",
         env={
             "START_DATE": "{{ (data_interval_start.in_timezone('Asia/Seoul') - macros.dateutil.relativedelta.relativedelta(weeks=2)) | ds }}",
-            "END_DATE":   "{{(data_interval_end.in_timezone('Asia/Seoul')
-                              - macros.dateutil.relativedelta.relativedelta(weeks=2)
-                              + macros.timedelta(days=5)) | ds}}"
+            "END_DATE": "{{(data_interval_end.in_timezone('Asia/Seoul')
+                            - macros.dateutil.relativedelta.relativedelta(weeks=2)
+                            + macros.timedelta(days=5)) | ds}}"
         },
         bash_command="echo \"START_DATE: $START_DATE\" && echo \"END_DATE: $END_DATE\""
     )
