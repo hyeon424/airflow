04c8582bbc14dbf2ed2c5c713b1d1896915b7575
hyeon424 <cold3311@gmail.com>
2025-08-27 16:37:22 +0900

    dags_bash_with_macro_ex1 & ex2 ver2

--- dags/dags_bash_with_macro_ex1.py
+++ dags/dags_bash_with_macro_ex1.py
@@ -10,14 +10,14 @@ from airflow.providers.standard.operators.bash import BashOperator
 with DAG(
     dag_id="dags_bash_with_macro_ex1",
     schedule="10 0 L * *",
-    start_date=pendulum.datetime(2025, 7, 1, tz="Asia/Seoul"),
-    catchup=False,
+    start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
+    catchup=True,
 ) as dag:
     # START_DATE: 전월 말일, END_DATE: 1일 전
     bash_task_1 = BashOperator(
         task_id="bash_task_1",
         env={
-            "START_DATE": "{{ data_interval_start.in_timezone('Asia/Seoul') | ds }}",
+            "START_DATE": "{{ (data_interval_start.in_timezone('Asia/Seoul') - macros.dateutil.relativedelta.relativedelta(months=1, days=1)) | ds }}",
             "END_DATE": "{{ (data_interval_end.in_timezone('Asia/Seoul') - macros.dateutil.relativedelta.relativedelta(days=1)) | ds }}"
         },
         bash_command="echo \"START_DATE: $START_DATE\" && echo \"END_DATE: $END_DATE\""
--- dags/dags_bash_with_macro_ex2.py
+++ dags/dags_bash_with_macro_ex2.py
@@ -10,15 +10,15 @@ from airflow.providers.standard.operators.bash import BashOperator
 with DAG(
     dag_id="dags_bash_with_macro_ex2",
     schedule="10 0 * * 6#2",
-    start_date=pendulum.datetime(2025, 7, 1, tz="Asia/Seoul"),
-    catchup=False,
+    start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
+    catchup=True,
 ) as dag:
     # START_DATE: 2주전 월요일, END_DATE: 2주전 토요일
     bash_task_2 = BashOperator(
         task_id="bash_task_2",
         env={
-            "START_DATE": "{{ (data_interval_start.in_timezone('Asia/Seoul') - macros.dateutil.relativedelta.relativedelta(weeks=2)) | ds }}",
-            "END_DATE": "{{ (data_interval_end.in_timezone('Asia/Seoul') - macros.dateutil.relativedelta.relativedelta(weeks=2) + macros.timedelta(days=5)) | ds }}"
+            "START_DATE": "{{ (data_interval_start.in_timezone('Asia/Seoul') - macros.dateutil.relativedelta.relativedelta(weeks=2) - macros.timedelta(days=5)) | ds }}",
+            "END_DATE": "{{ (data_interval_end.in_timezone('Asia/Seoul') - macros.dateutil.relativedelta.relativedelta(weeks=2)) | ds }}"
         },
         bash_command="echo \"START_DATE: $START_DATE\" && echo \"END_DATE: $END_DATE\""
     )
